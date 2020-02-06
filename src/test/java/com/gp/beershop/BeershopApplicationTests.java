14f74174d05baae490238216081eb3b868ae4535
sitdikov <l2m>
2020-02-06 16:39:51 +0300

    add User Stories

---
+++ src/test/java/com/gp/beershop/BeershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gp.beershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeershopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
