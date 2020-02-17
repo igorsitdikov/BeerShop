fd232fb32bb07553d0c6a9145870d9735faa6d6d
sitdikov <l2m>
2020-02-17 17:09:07 +0300

    added CustomerOrderMapperMockTest;

---
+++ src/test/java/com/gp/beershop/mapper/CustomerOrderMapperMockTest.java
@@ -0,0 +1,71 @@
+package com.gp.beershop.mapper;
+
+import com.gp.beershop.dto.CustomerOrder;
+import com.gp.beershop.entity.BeerEntity;
+import com.gp.beershop.entity.CustomerOrderEntity;
+import com.gp.beershop.mock.BeerMock;
+import com.gp.beershop.mock.OrderMock;
+import com.gp.beershop.repository.CustomerOrderRepository;
+import lombok.extern.java.Log;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+@SpringBootTest()
+@TestPropertySource("classpath:application-test.properties")
+@Log
+public class CustomerOrderMapperMockTest {
+    private final Integer ID = 1;
+
+    @SpyBean
+    private CustomerOrderMapper customerOrderMapper;
+
+    @Test
+    public void testCustomerOrderDtoToCustomerOrderEntity() {
+        final CustomerOrder customerOrder = OrderMock.getById(ID).getCustomerOrders().get(ID);
+
+        final CustomerOrderEntity customerOrderEntity = customerOrderMapper.sourceToDestination(customerOrder);
+
+        assertEquals(customerOrder.getCount(), customerOrderEntity.getCount());
+        assertEquals(customerOrder.getBeer().getId(), customerOrderEntity.getBeer().getId());
+        assertEquals(customerOrder.getBeer().getAlcohol(), customerOrderEntity.getBeer().getAlcohol());
+        assertEquals(customerOrder.getBeer().getName(), customerOrderEntity.getBeer().getName());
+        assertEquals(customerOrder.getBeer().getType(), customerOrderEntity.getBeer().getType());
+        assertEquals(customerOrder.getBeer().getInStock(), customerOrderEntity.getBeer().getInStock());
+        assertEquals(customerOrder.getBeer().getDescription(), customerOrderEntity.getBeer().getDescription());
+        assertEquals(customerOrder.getBeer().getCountry(), customerOrderEntity.getBeer().getCountry());
+        assertEquals(customerOrder.getBeer().getDensity(), customerOrderEntity.getBeer().getDensity());
+        assertEquals(customerOrder.getBeer().getPrice(), customerOrderEntity.getBeer().getPrice());
+    }
+    @Test
+    public void testCustomerOrderEntityToCustomerOrderDto() {
+        final CustomerOrder customerOrderExpected = OrderMock.getById(ID).getCustomerOrders().get(ID);
+
+        final BeerEntity beerEntity = new BeerEntity();
+        beerEntity.setName(customerOrderExpected.getBeer().getName());
+        beerEntity.setId(customerOrderExpected.getBeer().getId());
+        beerEntity.setAlcohol(customerOrderExpected.getBeer().getAlcohol());
+        beerEntity.setType(customerOrderExpected.getBeer().getType());
+        beerEntity.setCountry(customerOrderExpected.getBeer().getCountry());
+        beerEntity.setPrice(customerOrderExpected.getBeer().getPrice());
+        beerEntity.setDensity(customerOrderExpected.getBeer().getDensity());
+        beerEntity.setInStock(customerOrderExpected.getBeer().getInStock());
+        beerEntity.setDescription(customerOrderExpected.getBeer().getDescription());
+
+        final CustomerOrderEntity customerOrderEntity = new CustomerOrderEntity();
+        customerOrderEntity.setCount(customerOrderExpected.getCount());
+        customerOrderEntity.setBeer(beerEntity);
+
+        final CustomerOrder customerOrderActual = customerOrderMapper.destinationToSource(customerOrderEntity);
+
+        assertEquals(customerOrderExpected, customerOrderActual);
+    }
+}
