8285ef65958246626eac4f1b89597f289c2aaa5f
sitdikov <l2m>
2020-02-17 16:47:07 +0300

    resolved issue with sql request, table name "order" was reason, cause reserved word;

--- src/main/java/com/gp/beershop/entity/OrderEntity.java
+++ src/main/java/com/gp/beershop/entity/OrderEntity.java
@@ -7,6 +7,7 @@ import com.gp.beershop.dto.Orders;
 import com.gp.beershop.mapper.CustomerOrderMapper;
 import com.gp.beershop.mapper.UserMapper;
 import lombok.Data;
+import lombok.extern.java.Log;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -14,32 +15,37 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Data
-@Entity(name = "order")
+@Log
+@Entity(name = "orders")
 public class OrderEntity {
     @Transient
     private UserMapper userMapper;
     @Transient
     private CustomerOrderMapper customerOrderMapper;
-    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @ManyToOne
     @JoinColumn
     private UserEntity user;
     private Boolean processed;
     private Double total;
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "orders", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Set<CustomerOrderEntity> customerOrders;
-    public Orders convertToOrders () {
+
+    public Orders convertToOrders() {
         return Orders.builder()
-                .id(id)
-                .total(total)
-                .customer(Customer.builder()
-                        .id(1).build())
-                .processed(processed)
-//                .customerOrders(customerOrders.stream()
-//                .map(customerOrderMapper::destinationToSource).collect(Collectors.toList()))
-                .build();
+            .id(id)
+            .total(total)
+            .customer(Customer.builder()
+                          .id(1).build())
+            .processed(processed)
+            .customerOrders(
+                customerOrders.stream()
+                    .map(customerOrderMapper::destinationToSource).collect(Collectors.toList()))
+            .build();
     }
+
 //    @ManyToMany(mappedBy = "orders")
-//    private Set<BeerEntity> beers = new HashSet<>();
+//    private Set<CustomerOrderEntity> customerOrders = new HashSet<>();
 }
