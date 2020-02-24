96bad86bc068042557a975300e09f1c8a67494fd
sitdikov <l2m>
2020-02-24 14:23:33 +0300

    deleted comments from beershop.sql;

--- src/main/resources/db/changelog/beershop.sql
+++ src/main/resources/db/changelog/beershop.sql
@@ -1,28 +1,9 @@
-# DROP DATABASE beershop;
-# CREATE DATABASE beershop DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-# USE beershop;
-# DROP TABLE IF EXISTS beer;
-# CREATE TABLE beer
-# (
-#     id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-#     type        VARCHAR(512),
-#     in_stock    BOOL,
-#     name        VARCHAR(512),
-#     description VARCHAR(512),
-#     alcohol     DOUBLE,
-#     density     DOUBLE,
-#     country     VARCHAR(512),
-#     price       DOUBLE
-# );
-# ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-# ALTER TABLE beer
-#     CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
 INSERT INTO beer (type, in_stock, name, description, alcohol, density, country, price)
 VALUES ('светлое', true, 'Лидское', 'Лучшее пиво по бабушкиным рецептам', 5.0, 11.5, 'Республика Беларусь', 5.0),
        ('темное', true, 'Аливария', 'Пиво номер 1 в Беларуси', 4.6, 10.2, 'Республика Беларусь', 3.0),
        ('светлое осветлённое', true, 'Pilsner Urquell', 'непастеризованное', 4.2, 12.0, 'Чехия', 8.0);
 
-# DROP TABLE IF EXISTS user;
 CREATE TABLE user
 (
     id        INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -34,11 +15,10 @@ CREATE TABLE user
 );
 
 INSERT INTO user (name, password, email, phone, user_role)
-VALUES ('Иван Иванов', '123456', 'ivan.ivanov@mail.ru', '+375331234567', 1),
-       ('Петр Петров', '654321', 'petr.petrov@yandex.ru', '+375337654321', 1),
-       ('Алексей Алексеев', 'password', 'alexeev@gmail.com', '+375333021232', 2);
+VALUES ('Иван Иванов', '123456', 'ivan.ivanov@mail.ru', '+375331234567', 0),
+       ('Петр Петров', '654321', 'petr.petrov@yandex.ru', '+375337654321', 0),
+       ('Алексей Алексеев', 'password', 'alexeev@gmail.com', '+375333021232', 1);
 
-# DROP TABLE IF EXISTS orders;
 CREATE TABLE orders
 (
     id        INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -52,7 +32,6 @@ INSERT INTO orders (user_id, processed, total)
 VALUES (1, true, 25.0),
        (2, false, 27.0);
 
-# DROP TABLE IF EXISTS customer_order;
 CREATE TABLE customer_order
 (
     id       INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
